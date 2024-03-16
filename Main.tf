e4663749defb231d26030ac76c917519a0a41748
iamchiru1024 <146617017+iamchiru1024@users.noreply.github.com>
2024-03-16 13:57:55 +0530

    Update Main.tf

--- Main.tf
+++ Main.tf
@@ -38,7 +38,7 @@ resource "aws_instance" "web" {
   user_data              = templatefile("./install_jenkins.sh", {})
 
   tags = {
-    Name = "Jenkins-server
+    Name = "Jenkins-server"
   }
   root_block_device {
     volume_size = 30
